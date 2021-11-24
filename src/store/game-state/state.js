ffe6c795b7b9d27f57894a3e920652bf146d0aba
Toren Darby <darby@science.ubc.ca>
2021-11-23 23:52:18 -0800

    Fix board

--- src/store/game-state/state.js
+++ src/store/game-state/state.js
@@ -1,9 +1,15 @@
-import {NUM_ROWS_COLS} from 'src/store/game-state/constants';
+import { NUM_ROWS_COLS, NUM_SIZES } from "src/store/game-state/constants";
 
-export default function() {
+const board = Array.from(Array(NUM_ROWS_COLS), (_) =>
+  Array.from(Array(NUM_ROWS_COLS), (_) => Array(NUM_SIZES).fill(undefined))
+);
+// Temporary setup
+board[0][0][0] = 0;
+board[1][1][1] = 1;
+
+export default function () {
   return {
-    board: Array.from(Array(NUM_ROWS_COLS),
-      _ => Array(NUM_ROWS_COLS).fill([0, 0, undefined, 1])),
+    board: board,
     reserves: [],
   };
 }
