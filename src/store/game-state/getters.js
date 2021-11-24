a0402717a10017fa70499283708002cf6b595a7e
Toren Darby <darby@science.ubc.ca>
2021-11-23 23:51:45 -0800

    Add getLargestPiece getter

--- src/store/game-state/getters.js
+++ src/store/game-state/getters.js
@@ -1,3 +1,9 @@
-export const getPiece = (state) => (loc, size)  => {
-  return state.board[loc.row][loc.col][size]
-}
+export const getPiece = (state) => (loc, size) => {
+  return state.board[loc.row][loc.col][size];
+};
+
+export const getLargestPiece = (state) => (loc) => {
+  const space = state.board[loc.row][loc.col];
+  const largestSize = _.findLastIndex(space, (owner) => owner !== undefined);
+  return { size: largestSize, owner: space[largestSize] };
+};
