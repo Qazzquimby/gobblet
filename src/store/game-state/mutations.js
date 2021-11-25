0482b866a5d531f49b3bee7ee549a14a33a9d449
Toren Darby <darby@science.ubc.ca>
2021-11-24 22:00:49 -0800

    Add mutator for selectedSpace

--- src/store/game-state/mutations.js
+++ src/store/game-state/mutations.js
@@ -1,3 +1,7 @@
-export function setPiece (state, {loc, size, owner}) {
-  state.board[loc.row][loc.col][size] = owner
+export function setPiece(state, { loc, size, owner }) {
+  state.board[loc.row][loc.col][size] = owner;
+}
+
+export function setSelectedSpace(state, selectedSpace) {
+  state.selectedSpace = selectedSpace;
 }
