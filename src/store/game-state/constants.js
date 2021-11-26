328e255d97708fa1ca4c24258c27b043a6f1f881
Toren Darby <darby@science.ubc.ca>
2021-11-25 23:33:27 -0800

    Add reserves to state

--- src/store/game-state/constants.js
+++ src/store/game-state/constants.js
@@ -1,2 +1,4 @@
-export const NUM_ROWS_COLS = 4
-export const NUM_SIZES = 4
+export const NUM_ROWS_COLS = 4;
+export const NUM_SIZES = 4;
+export const NUM_PLAYERS = 2;
+export const NUM_RESERVE_STACKS = 3;
--- src/store/game-state/state.js
+++ src/store/game-state/state.js
@@ -1,4 +1,9 @@
-import { NUM_ROWS_COLS, NUM_SIZES } from "src/store/game-state/constants";
+import {
+  NUM_PLAYERS,
+  NUM_RESERVE_STACKS,
+  NUM_ROWS_COLS,
+  NUM_SIZES,
+} from "src/store/game-state/constants";
 
 const board = Array.from(Array(NUM_ROWS_COLS), (_) =>
   Array.from(Array(NUM_ROWS_COLS), (_) => Array(NUM_SIZES).fill(undefined))
@@ -7,11 +12,20 @@ const board = Array.from(Array(NUM_ROWS_COLS), (_) =>
 board[0][0][0] = 0;
 board[1][1][1] = 1;
 
+const reserves = Array.from(Array(NUM_PLAYERS), (_) =>
+  Array.from(Array(NUM_RESERVE_STACKS), (_) => Array(NUM_SIZES).fill(undefined))
+);
+reserves.forEach((playerReserve, player) => {
+  playerReserve.forEach((stack) => {
+    stack.fill(player);
+  });
+});
+
 export default function () {
   return {
     currentPlayer: 0,
     selectedSpace: undefined,
     board: board,
-    reserves: [],
+    reserves: reserves,
   };
 }
